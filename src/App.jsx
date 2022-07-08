1be73b21f8531a9025393a223c74d34b5d89be75
battaplus <battaplus@gmail.com>
2022-07-08 18:59:14 -0400

    punctuation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Verdi's Calculator</p>
+        <p>Verdi's Calculator!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
